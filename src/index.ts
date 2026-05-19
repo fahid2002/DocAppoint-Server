905ffee5d2b78ad3df7f36abcd0fd0c4bec3a855
fahid2002 <fahid23105101323.diu.edu.bd>
2026-05-20 05:13:52 +0600

    update index

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db";
 import authRoutes from "./routes/auth";
 import appointmentRoutes from "./routes/appointments";
+import reviewRoutes from "./routes/reviews";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -52,6 +53,7 @@ app.get("/", (_req, res) => {
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/appointments", appointmentRoutes);
+app.use("/api/reviews", reviewRoutes);
 
 // 404
 app.use((_req, res) => {
